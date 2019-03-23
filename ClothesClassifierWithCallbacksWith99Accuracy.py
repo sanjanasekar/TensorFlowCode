519856b194560bfca0d619b48958be89409f8f25
Sanjana Sekar <sanjanasekar29@gmail.com>
2019-03-22 17:50:03 -0700

    [TensorFlow]: Clothes Classifier using Fashin MNIST with 99% Accuracy using Callbacks

---
+++ ClothesClassifierWithCallbacksWith99Accuracy.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+
+
+class myCallBack(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs = {}):
+    if(logs.get('acc') >= 0.99):
+      print("\n Reached Accuracy level of 99% so cancelling the training")
+      self.model.stop_training = True
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+ 
+callbacks = myCallBack()
+  
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape = (28,28)),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=10, callbacks=[callbacks])
